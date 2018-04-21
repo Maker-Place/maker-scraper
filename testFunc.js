2dc9a0745e63257bae1dfca7d3f21e66b4399c20
Natalie Ryder <natalie@natalieryder.com>
2018-04-21 12:47:41 -0700

    test db connection

--- testFunc.js
+++ testFunc.js
@@ -1,4 +1,75 @@
+const mongoose = require("mongoose");
+const db = require("./models");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/news-scraper-heroku-test";
+mongoose.Promise = global.Promise;
+// This file empties the Books collection and inserts the memberships below
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/makerplace",
+    {
+        useMongoClient: true
+    }
+);
+
+
+const membershipsSeed = [
+    {
+        membership_type: "1 Year Prepaid",
+        annual_cost: "$1,296/year",
+        monthly_cost: "($52/month)",
+        discount: "(Save 33%)",
+        description: "Free Shop Orientation. After completion of the commitment period the membership will continue to automatically renew until member cancels. The member will be notified prior to any price increases."
+    },
+    {
+        membership_type: "6 Months Prepaid",
+        annual_cost: "$720/year",
+        monthly_cost: "$120/month",
+        discount: "(Save 25%)",
+        description: "Free Shop Orientation. After completion of the commitment period the membership will continue to automatically renew until member cancels. The member will be notified prior to any price increases."
+    },
+    {
+        membership_type: "3 Months Prepaid",
+        annual_cost: "$408/year",
+        monthly_cost: "($136/month)",
+        discount: "(Save 15%)",
+        description: "Free Shop Orientation. After completion of the commitment period the membership will continue to automatically renew until member cancels. The member will be notified prior to any price increases."
+    },
+    {
+        membership_type: "Month to Month",
+        annual_cost: "$160/month",
+        monthly_cost: "",
+        discount: "",
+        description: "Free Shop Orientation. After completion of the commitment period the membership will continue to automatically renew until member cancels. The member will be notified prior to any price increases."
+    },
+    {
+        membership_type: "Fabrication Pass",
+        annual_cost: "$79/month",
+        monthly_cost: "",
+        discount: "",
+        description: "Access to the craft room, assembly room, and 3D printing area. Membership will be automatically renewed and the associated debit/credit card will be charged at the current list price until cancelled. The member will be notified prior to any price increases."
+    },
+    {
+        membership_type: "10 Day Punch Card",
+        annual_cost: "$375/month",
+        monthly_cost: "",
+        discount: "",
+        description: "Valid for 10 days of membership and does not need to be used consecutively. Purchase of this pass does not include Shop Orientation or membership discounts on classes. Does not require any specific duration commitment. Promotional value valid for 1 year from date of purchase."
+    }
+];
+
 function sayHello() {
-    console.log('Hello');
+    db.Membership
+	.remove({})
+	.then (() => db.Membership.insertMany(membershipsSeed))
+    .then(data => {
+        data.map(item => (
+          console.log(item._id)
+        ));
+        process.exit(0);
+    })
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
+    });
 }
+
 sayHello();
